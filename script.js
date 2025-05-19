70a90a390228018d736523a5ead9693941fc4694
Guilherme Atanazio <loufar81@gmail.com>
2025-05-19 20:43:25 -0300

    primeiro

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('msgButton');
+const msgArea = document.getElementById('msgArea');
+
+button.addEventListener('click', () => {
+    msgArea.textContent = "Seja bem-vindo(a) ao meu site pessoal! Continue estudando e você vai longe! ;)";
+});
